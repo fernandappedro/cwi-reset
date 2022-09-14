158dd7599bd2f505bd4fe86a111fe45f13d037b5
Fernanda Pereira Pedro <fernandappedro@gmail.com>
2022-09-13 21:37:39 -0300

    adicionado projeto

---
+++ estrutura-base/scripts.js
@@ -0,0 +1,11 @@
+let nome = "Fernanda Pereira Pedro";
+console.log(nome);
+let idade = 29;
+console.log(idade);
+let altura = 166;
+console.log(altura);
+let ano = 2022;
+console.log(ano)
+let endereco = "Rua Taruma 55, Petropolis - Novo Hamburgo";
+console.log(endereco)
+
