34dff222db267f3ea844b909ce85ce8a08cff6c3
Fernanda Pereira Pedro <fernandappedro@gmail.com>
2022-09-15 23:09:33 -0300

    delete .ds_store

---
+++ Modulo 3 - aula 8/scripts.js
@@ -0,0 +1,17 @@
+let frutas = ['Maça', 'Banana', 'Laranja', 'Melancia', 'Manga', 'Jaca']
+
+let quantidadeDeFrutas = frutas.length
+
+let marcador = 0 
+
+while (marcador < quantidadeDeFrutas) {
+    console.log(frutas[marcador])
+
+    marcador = marcador+1
+}
+
+
+
+
+
+let valores = [1, 2, 3, 5, 8, 13, 21, 23, 34, 55]
